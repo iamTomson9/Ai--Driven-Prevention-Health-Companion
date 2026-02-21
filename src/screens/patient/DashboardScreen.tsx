5acb74d2b2af783f14cb1c2ad3ea6bd2170298e5
iamTomson9 <mtec20-001@thuto.bac.ac.bw>
2026-02-22 00:26:17 +0200

    intergrated database working to intergrate chatbot

--- src/screens/patient/DashboardScreen.tsx
+++ src/screens/patient/DashboardScreen.tsx
@@ -1,4 +1,3 @@
-// src/screens/patient/DashboardScreen.tsx
 import React, { useEffect, useState, useCallback } from 'react';
 import {
   View,
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDCeCLiM_tJ9XAVXC_frNww6Dkbepz1Shc",
+  authDomain: "ai-hcp267.firebaseapp.com",
+  projectId: "ai-hcp267",
+  storageBucket: "ai-hcp267.firebasestorage.app",
+  messagingSenderId: "215351716227",
+  appId: "1:215351716227:web:ad06aaef7d35542908eeb6",
 };
 
 const app = initializeApp(firebaseConfig);
